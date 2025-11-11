a91e0358ba7f5f5590f119a6a5034b6f289d9c44
vladpaun1 <vlad.paun06@gmail.com>
2025-11-11 22:35:31 +0100

    correct metadata import source

--- src/app/(app)/reports/page.tsx
+++ src/app/(app)/reports/page.tsx
@@ -1,4 +1,5 @@
-import type { Metadata, ReactNode } from "react";
+import type { Metadata } from "next";
+import type { ReactNode } from "react";
 
 import { addDays, differenceInCalendarDays, format } from "date-fns";
 
